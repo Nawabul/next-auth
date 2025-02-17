99ad9737527e7731b4c34cb52a9ea464485f35c6
Nawabul <nawabk693@gmail.com>
2025-02-17 13:25:08 +0530

    Add: dashboard in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,20 @@
 import {NextRequest, NextResponse } from 'next/server'
 export { default } from "next-auth/middleware"
- 
+ import { getToken } from 'next-auth/jwt'
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function middleware(request: NextRequest) {
+  const token = await getToken({req:request});
+  console.log('middleware token')
+  if (!token) {
+    return NextResponse.redirect(new URL('/signin', request.url))
+    
+  }
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
 	'/sign-in',
+  '/dashboard'
   ],
 }
